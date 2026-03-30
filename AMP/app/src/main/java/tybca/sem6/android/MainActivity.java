25013652f6ca4e13569e19aa4860ac2432d203dd
Hatim Juzer Kanorwala <hatimkanor21@gmail.com>
2026-03-30 19:34:03 +0530

    Android Code
    Android Code

---
+++ AMP/app/src/main/java/tybca/sem6/android/MainActivity.java
@@ -0,0 +1,66 @@
+package tybca.sem6.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText _main_et_username,_main_et_password;
+    Button _main_btn_login;
+    TextView _main_tv_forgotPassword;
+    String username,password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        _main_et_username = findViewById(R.id.main_et_username);
+        _main_et_password = findViewById(R.id.main_et_password);
+        _main_btn_login = findViewById(R.id.main_btn_login);
+        _main_tv_forgotPassword = findViewById(R.id.main_tv_forgotPassword);
+
+        _main_btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = _main_et_username.getText().toString().trim();
+                password = _main_et_password.getText().toString().trim();
+                if(username.isEmpty()){
+                    _main_et_username.setError("Enter Username");
+                }
+                else if(password.isEmpty()){
+                    _main_et_password.setError("Enter Password");
+                }
+                else if(password.length() <= 8){
+                    _main_et_password.setError("Password cannot be less than 8 characters");
+                }
+                else{
+                    if(username.equals("admin") && password.equals("admin@12345")){
+                        Intent i = new Intent(MainActivity.this,SecondActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Invalid Credentials, Try Again!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+    }
+}
