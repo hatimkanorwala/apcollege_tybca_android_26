72ef02e8abf647cc871fb4c7d63b62c8c3748e79
Hatim Juzer Kanorwala <hatimkanor21@gmail.com>
2026-04-09 13:48:02 +0530

    Lecture 8th March 2024 Commit
    Lecture 8th March 2024 Commit

--- AMP/app/src/main/java/tybca/sem6/android/MainActivity.java
+++ AMP/app/src/main/java/tybca/sem6/android/MainActivity.java
@@ -1,6 +1,8 @@
 package tybca.sem6.android;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +19,10 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     EditText _main_et_username,_main_et_password;
     Button _main_btn_login;
-    TextView _main_tv_forgotPassword;
+    TextView _main_tv_forgotPassword,_main_tv_newUser;
     String username,password;
+    public static final String myPreference = "myPrefs";
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +37,16 @@ public class MainActivity extends AppCompatActivity {
         _main_et_password = findViewById(R.id.main_et_password);
         _main_btn_login = findViewById(R.id.main_btn_login);
         _main_tv_forgotPassword = findViewById(R.id.main_tv_forgotPassword);
+        _main_tv_newUser = findViewById(R.id.main_tv_newUser);
+
+        sharedPreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE);
+        if(sharedPreferences.getAll().containsKey("isLoggedIn")){
+            if(sharedPreferences.getAll().containsKey("username")){
+                Intent i = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }
 
         _main_btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +64,10 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
                     if(username.equals("admin") && password.equals("admin@12345")){
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username",username);
+                        editor.putString("isLoggedIn","1");
+                        editor.commit();
                         Intent i = new Intent(MainActivity.this,SecondActivity.class);
                         startActivity(i);
                         finish();
@@ -60,7 +78,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
+    }
+    public void redirectToRegistration(View view){
+        Intent i = new Intent(MainActivity.this,Registration.class);
+        startActivity(i);
     }
 }
--- AMP/app/src/main/java/tybca/sem6/android/SecondActivity.java
+++ AMP/app/src/main/java/tybca/sem6/android/SecondActivity.java
@@ -1,15 +1,25 @@
 package tybca.sem6.android;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
-
+    public static final String myPreference = "myPrefs";
+    SharedPreferences sharedPreferences;
+    String username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,61 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        sharedPreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE);
+
+        if(sharedPreferences.getAll().containsKey("isLoggedIn")){
+            if(sharedPreferences.getAll().containsKey("username")){
+                username = sharedPreferences.getString("username","");
+                Toast.makeText(this, "Username: " + username, Toast.LENGTH_LONG).show();
+            }
+            else{
+                logout();
+            }
+        }
+        else{
+            logout();
+        }
+    }
+    public void btnLogout(View view){
+        logout();
+    }
+    public void logout(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.commit();
+        finish();
+        Intent i = new Intent(SecondActivity.this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int item_id = item.getItemId();
+        if(item_id == R.id.menu_home){
+
+        }
+        else if(item_id == R.id.menu_aboutus){
+
+        }
+        else if(item_id == R.id.menu_contactUs){
+
+        }
+        else if(item_id == R.id.menu_profile){
+
+        }
+        else if(item_id == R.id.menu_settings){
+
+        }
+        else if(item_id == R.id.menu_logout){
+            logout();
+        }
+        return true;
     }
 }
